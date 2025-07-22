a9eec54ea13ce759f0091abfd2aca5e9d8e3b7d1
sushobhit jain <sushobhitt.builtbyblank@gmail.com>
2025-07-22 12:06:34 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Sushobhit Jain on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
